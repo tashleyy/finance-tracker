f30905914a7eb0f0f918b3121736a909d7fb7ac0
Ashley Tjahjadi <ttjahjad@usc.edu>
2016-04-09 16:43:18 -0700

    add transaction backend

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,34 +1,32 @@
 var passport = require('passport');
 
 module.exports = {
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false
+  },
 
-    _config: {
-        actions: false,
-        shortcuts: false,
-        rest: false
-    },
+  login: function(req, res) {
+    passport.authenticate('local', function(err, user, info) {
+      if ((err) || (!user)) {
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      }
+      req.logIn(user, function(err) {
+        if (err) res.send(err);
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      });
+    })(req, res);
+  },
 
-    login: function(req, res) {
-        passport.authenticate('local', function(err, user, info) {
-            if ((err) || (!user)) {
-                return res.send({
-                    message: info.message,
-                    user: user
-                });
-            }
-            req.logIn(user, function(err) {
-                if (err) res.send(err);
-                return res.send({
-                    message: info.message,
-                    user: user
-                });
-            });
-
-        })(req, res);
-    },
-
-    logout: function(req, res) {
-        req.logout();
-        res.redirect('/');
-    },
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  },
 };
