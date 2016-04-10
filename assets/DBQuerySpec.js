e6a37aa312ded92e4062b5055cafe85de69f9583
Ashley Tjahjadi <ttjahjad@usc.edu>
2016-04-10 16:40:15 -0700

    fix account test

--- assets/DBQuerySpec.js
+++ assets/DBQuerySpec.js
@@ -1,11 +1,9 @@
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-
 describe('DBQuery', function() {
+  var ownerId;
   beforeEach(function(done) {
     login('ttjahjad@usc.edu', 'password', function(data) {
       if (data && data.user && data.user.id) {
-        var userId = btoa(data.user.id);
-        this.ownerId = userId;
+        ownerId = data.user.id;
         done();
       }
     });
@@ -25,28 +23,30 @@ describe('DBQuery', function() {
   });
   
   it('should be able to add Account', function(done) {
-    getAccounts(this.ownerId, function(accounts) {
+    getAccounts(ownerId, function(accounts) {
       // Test is only applicable if 'Checking' doesn't already exist
       var found = false;
       for (var i = 0; i < accounts.length; i++) {
         if (accounts[i].name == 'Checking') {
           found = true;
-          expect(true).toBeTruthy();
+          expect(found).toBeTruthy();
           done();
           break;
         }
       }
       if (!found) {
-        addAccount(this.ownerId, 'Checking', function(moreAccounts) {
+        addAccount(ownerId, 'Checking', function(moreAccounts) {
           accounts.forEach(function(element, index, array) {
             expect(moreAccounts).toContain(element);
           });
           for (var i = 0; i < moreAccounts.length; i++) {
-            if (accounts[i].name == 'Checking') {
-              done();
+            if (moreAccounts[i].name == 'Checking') {
+              found = true;
+              break;
             }
           }
-          expect(false).toBeTruthy();
+          expect(found).toBeTruthy();
+          done();
         });
       }
     });
