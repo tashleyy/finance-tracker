edf296dea65c551f6147ce3a35474fa94107e4f6
Ashley Tjahjadi <ttjahjad@usc.edu>
2016-04-17 23:11:44 -0700

    fixing according to comments

--- features/step_definitions/transactions_definitions.js
+++ features/step_definitions/transactions_definitions.js
@@ -1,5 +1,3 @@
-/* eslint new-cap: "off" */
-
 module.exports = function() {
   this.When(/^I upload a file with valid data$/, function(cb) {
     // Write code here that turns the phrase above into concrete actions
