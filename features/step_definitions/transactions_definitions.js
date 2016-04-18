8dadbb443282f0b1e6fbb08c56c63da8682a73f5
Ashley Tjahjadi <ttjahjad@usc.edu>
2016-04-17 23:16:02 -0700

    fixed according to the comments

--- features/step_definitions/transactions_definitions.js
+++ features/step_definitions/transactions_definitions.js
@@ -1,6 +1,12 @@
 module.exports = function() {
-  this.When(/^I upload a file with valid data$/, function(cb) {
-    // Write code here that turns the phrase above into concrete actions
-    cb(null, 'pending');
+  this.When(/^I upload a file called (.*) to selector (.*)$/, function(filename, selector,cb) {
+  	this.attachFile(selector, filename);
+   cb();
   });
+
+  this.Then(/^I should be on the dashboard page$/, {timeout: 30 * 1000}, function(cb) {
+    this.assertUrl('http://localhost:1337/dashboard/');
+    cb();
+  });
+
 };
