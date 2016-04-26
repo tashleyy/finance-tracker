4238af5d00f631b77754276a3ddec48d462d985c
Ashley Tjahjadi <ttjahjad@usc.edu>
2016-04-25 22:06:23 -0700

    add graph totals

--- assets/js/dbquery.js
+++ assets/js/dbquery.js
@@ -143,3 +143,89 @@ function getAccountGraphData(ownerId, accountName, cb) {
     });
   });
 }
+
+function getAllAccountGraphData(ownerId, cb) {
+  getAccounts(ownerId, function(accounts) {
+    var finalTotalAssets = [];
+    var finalTotalLiabilities = [];
+    var finalTotalNetWorth = [];
+    var allAccounts = [];
+    var counters = [];
+    var lastAssets = [];
+    var lastLiabilities = [];
+    accounts.forEach(function(value, index, array) {
+      getAccountGraphData(ownerId, value.name, function(finalData) {
+        finalData.sort(function(a, b) {
+          if (a.date < b.date) { return -1; }
+          if (a.date > b.date) { return 1; }
+          return 0;
+        });
+        allAccounts.push(finalData);
+        counters.push(0);
+        lastAssets.push(0);
+        lastLiabilities.push(0);
+        if (allAccounts.length && allAccounts.length === array.length) {
+          var done = true;
+          for (let i = 0; i < allAccounts.length; i++) {
+            if (counters[i] < allAccounts[i].length) {
+              done = false;
+              break;
+            }
+          }
+          while (!done) {
+            let minDate;
+            for (let i = 0; i < allAccounts.length; i++) {
+              if (counters[i] < allAccounts[i].length && (!minDate || allAccounts[i][counters[i]].date < minDate)) {
+                minDate = allAccounts[i][counters[i]].date;
+              }
+            }
+            var newAssets = 0;
+            var newLiabilities = 0;
+            for (let i = 0; i < allAccounts.length; i++) {
+              if (counters[i] < allAccounts[i].length && dateEqual(allAccounts[i][counters[i]].date, minDate)) {
+                if (allAccounts[i][counters[i]].balance >= 0) {
+                  lastAssets[i] = allAccounts[i][counters[i]].balance;
+                  lastLiabilities[i] = 0;
+                } else {
+                  lastAssets[i] = 0;
+                  lastLiabilities[i] = allAccounts[i][counters[i]].balance;
+                }
+                counters[i]++;
+              }
+              newAssets += lastAssets[i];
+              newLiabilities += lastLiabilities[i];
+            }
+            finalTotalAssets.push({
+              date: new Date(minDate),
+              balance: newAssets
+            });
+            finalTotalLiabilities.push({
+              date: new Date(minDate),
+              balance: newLiabilities
+            });
+            done = true;
+            for (let i = 0; i < allAccounts.length; i++) {
+              if (counters[i] < allAccounts[i].length) {
+                done = false;
+                break;
+              }
+            }
+          }
+          for (let i = 0; i < finalTotalAssets.length; i++) {
+            finalTotalNetWorth.push({
+              date: new Date(finalTotalAssets[i].date),
+              balance: Number(finalTotalAssets[i].balance) + Number(finalTotalLiabilities[i].balance)
+            });
+          }
+          cb(finalTotalAssets, finalTotalLiabilities, finalTotalNetWorth);
+        }
+      });
+    });
+  });
+
+  function dateEqual(date1, date2) {
+    var a = new Date(date1);
+    var b = new Date(date2);
+    return (a.getYear() === b.getYear() && a.getMonth() === b.getMonth() && a.getDate() === b.getDate());
+  }
+}
--- assets/js/graph.js
+++ assets/js/graph.js
@@ -12,8 +12,10 @@ var parseTime = d3.time.format('%Y-%m-%dT%H:%M:%S.%LZ');
 var line = d3.svg.line()
   .x(function(d) { return x(d.date); })
   .y(function(d) { return y(d.balance); });
+var minY;
+var maxY;
 
-function setupGraph() {
+function setupGraph(ownerId) {
   svg = d3.select('#graph').append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
@@ -52,14 +54,71 @@ function setupGraph() {
   var past = new Date();
   past.setMonth(past.getMonth() - 6);
   x.domain([past, today]);
-  y.domain([0, 500]);
+  y.domain([-500, 500]);
   svg.select('.xAxis').call(xAxis);
   svg.select('.yAxis').call(yAxis);
   svg.selectAll('path.line').attr('d', line);
+
+  getAllAccountGraphData(ownerId, function(totalAssetsData, totalLiabilitiesData, totalNetWorthData) {
+    for (let i = 0; i < totalAssetsData.length; i++) {
+      if (!minY || totalAssetsData[i].balance < minY) {
+        minY = Number(totalAssetsData[i].balance);
+      }
+      if (!maxY || totalAssetsData[i].balance > maxY) {
+        maxY = Number(totalAssetsData[i].balance);
+      }
+    }
+    for (let i = 0; i < totalLiabilitiesData.length; i++) {
+      if (!minY || totalLiabilitiesData[i].balance < minY) {
+        minY = Number(totalLiabilitiesData[i].balance);
+      }
+      if (!maxY || totalLiabilitiesData[i].balance > maxY) {
+        maxY = Number(totalLiabilitiesData[i].balance);
+      }
+    }
+    for (let i = 0; i < totalNetWorthData.length; i++) {
+      if (!minY || totalNetWorthData[i].balance < minY) {
+        minY = Number(totalNetWorthData[i].balance);
+      }
+      if (!maxY || totalNetWorthData[i].balance > maxY) {
+        maxY = Number(totalNetWorthData[i].balance);
+      }
+    }
+    y.domain([minY, maxY]);
+    svg.select('.yAxis').call(yAxis);
+    var newLine = svg.append('path')
+      .datum(totalAssetsData)
+      .attr('class', 'line')
+      .attr('id', 'totalAssetsLine')
+      .attr('clip-path', 'url(#clip)')
+      .attr('d', line);
+    var newLine2 = svg.append('path')
+      .datum(totalLiabilitiesData)
+      .attr('class', 'line')
+      .attr('id', 'totalLiabilitiesLine')
+      .attr('clip-path', 'url(#clip)')
+      .attr('d', line);
+    var newLine3 = svg.append('path')
+      .datum(totalNetWorthData)
+      .attr('class', 'line')
+      .attr('id', 'totalNetWorthLine')
+      .attr('clip-path', 'url(#clip)')
+      .attr('d', line);
+  });
 }
 
 function graphAccount(ownerId, accountName) {
   getAccountGraphData(ownerId, accountName, function(finalData) {
+    for (let i = 0; i < finalData.length; i++) {
+      if (!minY || finalData.balance < minY) {
+        minY = Number(finalData.balance);
+      }
+      if (!maxY || finalData.balance > maxY) {
+        maxY = Number(finalData.balance);
+      }
+    }
+    y.domain([minY, maxY]);
+    svg.select('.yAxis').call(yAxis);
     var newLine = svg.append('path')
       .datum(finalData)
       .attr('class', 'line')
